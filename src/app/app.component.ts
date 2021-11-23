5dc4245f13e6bb5247a078595db806b8741e6322
Richard Nguyen <Richard.Nguyen@nafinc.com>
2021-11-23 14:00:13 -0800

    Add SignalR Connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalRService } from './services/signal-r.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'SignalR';
+export class AppComponent implements OnInit {
+  constructor(
+    public signalRService: SignalRService,
+    private http: HttpClient
+  ) {}
+  ngOnInit() {
+    this.signalRService.startConnection();
+    this.signalRService.addSMSNotificationListener();
+  }
+
+  public click(message: string) {
+    this.signalRService.broadcastData(message);
+  }
 }
---
+++ src/app/services/signal-r.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr'; // or from "@microsoft/signalr" if you are using a new library
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignalRService {
+  private hubConnection: signalR.HubConnection;
+
+  public startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:53970/notifications')
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection
+      .start()
+      .then(() =>
+        console.log(`Connection started ${this.hubConnection.connectionId}`)
+      )
+      .catch((err) => console.log('Error while starting connection: ' + err));
+  };
+
+  public broadcastData = (message: string) => {
+    this.hubConnection
+      .invoke('broadcastData', message)
+      .catch((err) => console.error(err));
+  };
+
+  public addSMSNotificationListener = () => {
+    this.hubConnection.on('sms-notification', (data) => {
+      console.log('Received Data:', data);
+    });
+  };
+}
